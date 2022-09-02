b17e965d8430e1a5182984c8cbe05759cdae6743
Peter Chen <peterchen2017125@gmail.com>
2022-09-02 18:27:30 +0800

    20220902

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,10 @@
 <?php
 
-$host = 'localhost';
-$dbuser = 'admin';
-$dbpwd = '12345678';
-$dbname = 'webphp';
+$host = 'ec2-176-34-215-248.eu-west-1.compute.amazonaws.com
+';
+$dbuser = 'sglprqemikcdpm';
+$dbpwd = '6a67374e4c6380173a80354484b6d9736115f608d76ecf9e9ddd4f263d6cbf29';
+$dbname = 'd6qqgu02k3hpvo';
 
 $conn = mysqli_connect($host,$dbuser,$dbpwd,$dbname);
 
